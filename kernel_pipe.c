b1f5cf7568d3b3ab8258ac1067f898915dbbdbec
Evan Katsoupis <48357575+EvanKat@users.noreply.github.com>
2020-12-01 23:15:26 +0200

    kernel_pipe.c
    kernel_pipe.c contains all the basic functions that pipe functionality needs.

--- kernel_pipe.c
+++ kernel_pipe.c
@@ -1,9 +1,20 @@
-
 #include "tinyos.h"
-
 
 int sys_Pipe(pipe_t* pipe)
 {
 	return -1;
 }
+int pipe_write(void* pipecb_t, const char *buf, unsigned int n){
+	return -1;
+}
+int pipe_read(void* pipecb_t, char *buf, unsigned int n){
+	return -1;
+}
 
+int pipe_writer_close(void* _pipecb){
+	return -1;
+}
+
+int pipe_reader_close(void* _pipecb){
+	return -1;
+}
