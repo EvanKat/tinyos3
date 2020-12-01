d22f03630b61ae945e6133d43218826929675dc6
Evan Katsoupis <48357575+EvanKat@users.noreply.github.com>
2020-12-01 23:17:02 +0200

    Update kernel_pipe.h

--- kernel_pipe.h
+++ kernel_pipe.h
@@ -9,3 +9,13 @@
 typedef struct pipe_control_block {
     FCB *reader, *writer;
 } Pipe_cb;
+
+int sys_Pipe(pipe_t* pipe);
+
+int pipe_write(void* pipecb_t, const char *buf, unsigned int n);
+
+int pipe_read(void* pipecb_t, char *buf, unsigned int n);
+
+int pipe_writer_close(void* _pipecb);
+
+int pipe_reader_close(void* _pipecb);
