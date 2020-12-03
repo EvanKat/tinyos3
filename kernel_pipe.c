8b701c9151eb637c67c9cbdfb174731a28fbcd85
Evan Katsoupis <48357575+EvanKat@users.noreply.github.com>
2020-12-03 21:56:45 +0200

    Update
    Set Pipe Control Block initialization.

--- kernel_pipe.c
+++ kernel_pipe.c
@@ -1,7 +1,41 @@
 #include "tinyos.h"
+#include "kernel_pipe.h"
+
+
+static file_ops readOperations = {
+	.Open = NULL,
+	.Read = pipe_read,
+	.Write = NULL,
+	.Close = pipe_close_reade;
+};
+
+static file_ops writeOperations = {
+	.Open = NULL,
+	.Read = NULL,
+	.Write = pipe_write,
+	.Close = pipe_close_writer
+};
+
+Pipe_CB* pipe_init() {
+
+	Pipe_CB* new_Pipe_CB = xmalloc(sizeof(Pipe_CB)); // Space allocation of the new pipe control block
+	
+	new_Pipe_CB->reader = NULL;
+	new_Pipe_CB->writer = NULL;
+
+	new_Pipe_CB->w_position = 0;
+	new_Pipe_CB->r_position = 0;
+
+	new_Pipe_CB->has_space = COND_INIT;
+	new_Pipe_CB->has_data = COND_INIT;
+
+	return new_Pipe_CB;
+}
+
 
 int sys_Pipe(pipe_t* pipe)
 {
+
 	return -1;
 }
 int pipe_write(void* pipecb_t, const char *buf, unsigned int n){
